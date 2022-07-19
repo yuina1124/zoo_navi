1626dd5f7bf91d7d8958a706c01b208c1fb096f8
yuina1124 <yuina1124oreo@gmail.com>
2022-07-19 07:24:10 +0000

    [Finish]user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = current_user
     @zoos = @user.zoos
@@ -7,12 +7,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction)
+    params.require(:user).permit(:name, :birthday)
   end
 
 end
